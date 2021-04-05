21777e24162a78643b387c542cb6cfafa2adb59a
Ryo-exid <sakura4oneok@gmail.com>
2021-04-05 13:46:04 +0000

    [Fix]bookのクリエイト機能の作成

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,17 +1,20 @@
 class BooksController < ApplicationController
   def index
+    @books = Book.all
   end
 
   def show
+    @book = Book.find(params[:id])
   end
 
   def new
+    @book = Book.new
   end
 
   def create
     @book = Book.new(book_params)
     book.save
-    redirect_to books_path
+    redirect_to books_path(book.id)
   end
 
   def edit
