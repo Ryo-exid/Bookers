65faccb03856b593c9e064a91731a4b7e7190908
Ryo-exid <sakura4oneok@gmail.com>
2021-04-25 08:00:48 +0000

    [Add]開発スキルアップ/バッチ処理の実装/削除プログラムの作成

--- config/application.rb
+++ config/application.rb
@@ -8,8 +8,7 @@ Bundler.require(*Rails.groups)
 
 module Bookers
   class Application < Rails::Application
-    # Initialize configuration defaults for originally generated Rails version.
-    config.load_defaults 5.2
+    config.paths.add 'lib', eager_load: true  # この行を追加
 
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
